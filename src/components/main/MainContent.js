1dcc600ea9b2272fee9d371f83308e617cd1b240
Michele M <mnmin@gmail.com>
2022-07-13 12:10:46 +0100

    Changes css leftMenu

--- src/components/main/MainContent.js
+++ src/components/main/MainContent.js
@@ -1,7 +1,3 @@
-// import rubbishButton from "../../assets/icons/rubbish-bin-delete-button.png";
-// import rateStarButton from "../../assets/icons/rate-star-button.png";
-// import backArrow from "../../assets/icons/back-arrow.png";
-// import flaticonWelcomeImage from "../../assets/images/flaticon-welcome-image.png";
 import Title from "./Title";
 import Header from "./Header";
 import EmailBody from "./EmailBody";
